76dd23b0675bfa616036c33da7b77fff1697ba6a
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-10-20 08:16:44 -0400

    Code refactoring (removed unnecessary else)

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -11,14 +11,15 @@ const assertArraysEqual = function(actual, expected) {
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
   }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  
+  return true;
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
