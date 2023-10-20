248f13507c014c7934e9e513ee0242746cad3ce6
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-10-19 22:54:29 -0400

    Add assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,34 @@
+const assertArraysEqual = function(actual, expected) {
+  const result = eqArrays(actual,expected);
+
+  if (result) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else if (arr1.length === 0) {
+    return true;
+  }  else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual([], []);
+assertArraysEqual([1], [1]);
+assertArraysEqual([], [2]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+assertArraysEqual(["1", "2", "3", "4"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3", "4"]);
