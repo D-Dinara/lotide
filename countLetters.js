dadfee69d1f1087da08029a13f2dacd651406413
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-10-24 12:22:13 -0400

    Add letterPositions function

--- countLetters.js
+++ countLetters.js
@@ -10,6 +10,8 @@ const assertEqual = function(actual, expected) {
 // The countLetters function counts letters in a string and returns an object with characters and their quantity in the string
 const countLetters = function(str) {
   let result = {};
+
+  // Remove spaces from the string before counting letters
   let noSpacesStr = str.split(" ").join("");
 
   for (const char of noSpacesStr) {
