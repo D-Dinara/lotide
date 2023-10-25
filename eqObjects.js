60b2ff6b20415afc3eebc845ea1d34b6ced7fdfd
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-10-25 11:36:30 -0400

    add eqObjects function

---
+++ eqObjects.js
@@ -0,0 +1,62 @@
+//If the values are arrays check arrays' elements for equality
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+//Test the results
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Returns true if both objects have identical keys with identical values.
+const eqObjects = function(object1, object2) {
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+
+  //Check if the number of keys in objects are equal
+  if (object1Keys.length !== object2Keys.length) {
+    return false;
+  }
+
+  for (const key of object1Keys) {
+
+    //Check if the value is an array
+    if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
+      eqArrays(object1[key],object2[key]);
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+//Testing the function
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertEqual(eqObjects(shirtObject, anotherShirtObject),true);
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject),false);
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
+
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false);
--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,3 +1,5 @@
+// Testing results for equality
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -6,6 +8,7 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+//The function returns a key on an object where its value matches a given value.
 const findKeyByValue = function(obj,value) {
 
   for (const key in obj) {
@@ -15,6 +18,7 @@ const findKeyByValue = function(obj,value) {
   }
 };
 
+//Testing the function
 const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
