4fb14eb0574a30eb9f640821098c1de2bbf92a78
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-10-25 16:04:28 -0400

    Add comments to map function test cases

--- map.js
+++ map.js
@@ -42,11 +42,17 @@ const assertArraysEqual = function(actual, expected) {
 
 
 //Testing the map function
+
+// Testing the map function with a callback that returns the first character of each word
 const words = ["ground", "control", "to", "major", " "];
 const results1 = map(words, word => word[0]);
+
+// Testing the map function with a callback that prepends "this is " to each word
 const results2 = map(words, word => "this is " + word);
 assertArraysEqual(["g","c","t","m"," "], results1);
 assertArraysEqual(["this is ground", "this is control", "this is to", "this is major", "this is  "], results2);
+
+// Testing the map function with a callback that multiplies each number by 2
 const numbers = [1, 2, 3, 4];
 const results3 = map(numbers, number => number * 2);
 assertArraysEqual([2, 4, 6, 8], results3);
