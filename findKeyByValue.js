940c8e592e920bbf983bf890dd3ee74c7594243d
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-10-25 10:15:10 -0400

    Add findKeyByVale function

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj,value) {
+  let result = "";
+
+  for (const key in obj) {
+    if (obj[key] === value) {
+      result = key;
+    }
+  }
+
+  if (result) return result;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
