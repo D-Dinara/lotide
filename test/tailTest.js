5a346e8a990ac3520d70e100190407f9730033d5
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-11-07 12:03:59 -0500

    Code refactor. Add comment to tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -7,6 +7,7 @@ assertEqual(result.length, 2);
 assertEqual(result[0], "Lighthouse");
 assertEqual(result[1], "Labs");
 
+// Test Case: Check that the original array is not modified after the function is called
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
 assertEqual(words.length, 3);
