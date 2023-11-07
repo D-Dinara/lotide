68895a224e7696e87395c9d9428db09801c44ebe
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-11-07 11:59:53 -0500

    Code refactor. Add comments to headTest and tailTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,12 @@
 const assertEqual = require('../assertEqual');
 const head = require('../head');
 
+//Check if the head function returns the first element of an array
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+//Test case: an array with one element
 assertEqual(head(["0"]), "0");
+
+//Test case: an empty array. The function should return undefined
 assertEqual(head([]), undefined);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,7 @@
 const tail = require('../tail');
 const assertEqual = require('../assertEqual');
 
+// Test Case: Check the returned array
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2);
 assertEqual(result[0], "Lighthouse");
@@ -10,8 +11,10 @@ const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
 assertEqual(words.length, 3);
 
+//Test case: an array with one element. The function should return an empty array.
 const test3 = tail(["Hello"]);
 assertEqual(test3.length, 0);
 
+//Test case: empty array. The tail function should return an empty array
 const test4 = tail([]);
 assertEqual(test4.length, 0);
