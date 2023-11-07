1e92259b7ddaa6f5aa45605a6c41ad3e08c16736
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-11-07 15:13:01 -0500

    Code refactor. Add comments to middleTest

--- middle.js
+++ middle.js
@@ -1,26 +1,21 @@
 //Implementing middle function. The function takes in an array and returns the middle-most element(s) of the given array.
 const middle = function(array) {
-  
-  //Define an array with middle elements
-  let middleArray = [];
 
   //Check if arrays length is less than 3. If it is return an empty array
   if (array.length < 3) {
-    return middleArray;
+    return [];
     //Check if the array's length is an even number
     //If even push two middle elements
   } else if (array.length % 2 === 0) {
     const midIndex1 = array.length  / 2 - 1;
     const midIndex2 = array.length / 2;
-    middleArray.push(array[midIndex1]);
-    middleArray.push(array[midIndex2]);
+    return [array[midIndex1], array[midIndex2]];
+ 
     //If not even push one middle element
   } else {
     const midIndex = Math.floor(array.length / 2);
-    middleArray.push(array[midIndex]);
+    return [array[midIndex]];
   }
- 
-  return middleArray;
 };
 
 module.exports = middle;
--- test/middleTest.js
+++ test/middleTest.js
@@ -2,9 +2,14 @@ const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
 //Testing middle function
+//Test case: array's length is an even number
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+
+//Test case: array's length is an odd number
 assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
 assertArraysEqual(middle([1, 2, 3]), [2]);
+
+//Test case: array's length less than 3
 assertArraysEqual(middle([1]), []);
 assertArraysEqual(middle([1, 2]), []);
