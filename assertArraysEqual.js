06d7dada860748560d18ef7c304ad28e8e0bcbbf
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-11-07 15:03:48 -0500

    Add middleTest

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,8 @@
-// The eqArrays function takes in two arrays as arguments and returns true if all elements of two arrays are equal
 const eqArrays = require('./eqArrays');
-
 
 //The assertArraysEqual function takes in two arrays to compare to each other and logs a message depending on the result of eqArrays' function output (result).
 const assertArraysEqual = function(actual, expected) {
-  const result = eqArrays(actual,expected);
-  if (result) {
+  if (eqArrays(actual,expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`⛔⛔⛔ Assertion Failed: ${actual} !== ${expected}`);
--- middle.js
+++ middle.js
@@ -1,46 +1,20 @@
-// Testing arrays for equality
-//The assertArraysEqual function takes in two values to compare to each other and logs a message depending on the result of eqArrays' functioun output (result): if result is true - the assertion passed, if false - failed.
-
-const assertArraysEqual = function(actual, expected) {
-  const result = eqArrays(actual,expected);
-
-  if (result) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔⛔⛔ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// Comparing arrays' elements with each other.
-// The eqArrays function takes in two arrays as arguments and returns true if all elements of two arrays are equal and returns false if the elements are not equal
-const eqArrays = function(arr1, arr2) {
-
-  //Comparing the arrays' lengths first
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  //Comparing each element
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
 //Implementing middle function. The function takes in an array and returns the middle-most element(s) of the given array.
 const middle = function(array) {
+  
+  //Define an array with middle elements
   let middleArray = [];
 
+  //Check if arrays length is less than 3. If it is return an empty array
   if (array.length < 3) {
     return middleArray;
+    //Check if the array's length is an even number
+    //If even push two middle elements
   } else if (array.length % 2 === 0) {
     const midIndex1 = array.length  / 2 - 1;
     const midIndex2 = array.length / 2;
     middleArray.push(array[midIndex1]);
     middleArray.push(array[midIndex2]);
+    //If not even push one middle element
   } else {
     const midIndex = Math.floor(array.length / 2);
     middleArray.push(array[midIndex]);
@@ -49,12 +23,4 @@ const middle = function(array) {
   return middleArray;
 };
 
-
-
-//Testing middle function
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+module.exports = middle;
