ae6e78952a1ae085241c646cf02ada33fbcdf295
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-11-08 17:42:09 -0500

    Refactor eqArrays function to handle nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -6,11 +6,16 @@ const eqArrays = function(arr1, arr2) {
   }
   //Check each element
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      // If both elements are arrays, recursively compare them
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
-
   return true;
 };
 
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -5,41 +5,55 @@ describe("#eqArrays", () => {
 
   //Test case: equal arrays of numbers
   it("returns true for [1, 2, 3], [1, 2, 3]", () => {
-    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+    assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]));
   });
 
   //Test case: not equal arrays of numbers
   it("returns false for [1, 2, 3], [3, 2, 1]", () => {
-    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+    assert.isFalse(eqArrays([1, 2, 3], [3, 2, 1]));
   });
 
   //Test case: empty arrays
   it("returns true for [], []", () => {
-    assert.strictEqual(eqArrays([], []), true);
+    assert.isTrue(eqArrays([], []));
   });
 
   //Test case: an empty array and an array with one element
   it("returns false for [], [2]", () => {
-    assert.strictEqual(eqArrays([], [2]), false);
+    assert.isFalse(eqArrays([], [2]));
   });
 
   //Test case: arrays with strings
   it("returns true for ['1', '2', '3'], ['1', '2', '3']", () => {
-    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+    assert.isTrue(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
   });
   
   //Test case: arrays with different types of data
   it("returns false for ['1', '2', '3'], ['1', '2', 3]", () => {
-    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+    assert.isFalse(eqArrays(["1", "2", "3"], ["1", "2", 3]));
   });
 
   //Test case: arrays with different number of elements
   it("returns false for ['1', '2', '3', '4'], ['1', '2', 3]", () => {
-    assert.strictEqual(eqArrays(["1", "2", "3", "4"], ["1", "2", "3"]), false);
+    assert.isFalse(eqArrays(["1", "2", "3", "4"], ["1", "2", "3"]));
   });
 
   //Test case: arrays with different number of elements
   it("returns false for ['1', '2', '3'], ['1', '2', '3', '4']", () => {
-    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", "3",  "4"]), false);
+    assert.isFalse(eqArrays(["1", "2", "3"], ["1", "2", "3",  "4"]));
+  });
+
+  //Testing nested arrays:
+  
+  it("returns true for identical nested arrays", () => {
+    assert.isTrue(eqArrays([[2, 3], [4]], [[2, 3], [4]]));
+  });
+
+  it("returns false for different nested arrays", () => {
+    assert.isFalse(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]));
+  });
+
+  it("returns false for different nested arrays (values on differents levels)", () => {
+    assert.isFalse(eqArrays([[2, 3], [4]], [[2, 3], 4]));
   });
 });
